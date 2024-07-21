0cb29451c2f123d5c7502a683116b1e71c2732a4
Jean <20massalia@kakao.com>
2024-07-22 01:04:04 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    s = s.toLowerCase();
+
+    let words = s.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > 0) {
+            words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+        }
+    }
+
+    return words.join(' ');
+}

9f61599a8e244db3970b62751d59add59c5921a4
Jean <20massalia@kakao.com>
2025-04-20 20:53:27 +0900

    [level 2] Title: 예상 대진표, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,11 @@
+function solution(n, a, b) {
+    let cnt = 1;
+
+    while (Math.abs(a - b) !== 1 || Math.max(a, b) % 2 !== 0) {
+        a = Math.ceil(a / 2);
+        b = Math.ceil(b / 2);
+        cnt++;
+    }
+
+    return cnt;
+}

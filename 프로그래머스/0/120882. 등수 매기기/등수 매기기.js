e607d35b80df553db07ae4f4d8f399722affb716
Jean <20massalia@kakao.com>
2025-04-24 20:35:29 +0900

    [level 0] Title: 등수 매기기, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,6 @@
+function solution(score) {
+    const averages = score.map(([a, b]) => (a + b) / 2);
+    const sorted = [...averages].sort((a, b) => b - a);
+
+    return averages.map(avg => sorted.indexOf(avg) + 1);
+}

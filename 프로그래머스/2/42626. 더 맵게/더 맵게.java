d4d219cf68801f8bfca91962085dd238fea8402a
Jean <20massalia@kakao.com>
2026-08-21 11:14:36 +0900

    [level 2] Title: 더 맵게, Time: 676.12 ms, Memory: 140 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int s : scoville) pq.add(s);
+        
+        while (pq.peek() < K) {
+            if (pq.size() == 1) return -1;
+            
+            int first = pq.poll();
+            int second = pq.poll();
+            
+            int newScov = first + second * 2;
+            pq.add(newScov);
+            
+            answer++;
+        }
+        
+        return answer;
+    }
+}

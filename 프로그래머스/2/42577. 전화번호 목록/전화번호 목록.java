beaef9bb037963fbf86b20f8da64afca622fc168
Jean <20massalia@kakao.com>
2026-09-22 10:55:28 +0900

    [level 2] Title: 전화번호 목록, Time: 175.14 ms, Memory: 237 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        HashSet<String> set = new HashSet<>(Arrays.asList(phone_book));
+        
+        for (String phone : phone_book) {
+            for (int j = 1; j < phone.length(); j++) {
+                if (set.contains(phone.substring(0, j))) return false;
+            }
+        }
+        
+        return true;
+    }
+}
